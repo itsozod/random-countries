e3bd4f42360badb5b6e249572b7e4bf32dfc90d7
Ozod <bukhorizodao@gmail.com>
2023-08-07 17:21:50 +0500

    prevent the button from clicking many times

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const countryContainer = document.querySelector(".country-container");
 const card = document.querySelector(".card");
 const btn = document.querySelector(".btn");
-const h3 = document.querySelector("h3");
+const generateMessage = document.querySelector(".generate");
 
 async function showCountry() {
+  console.log("Button is clicked");
+  btn.removeEventListener("click", showCountry);
   try {
     const response = await fetch(" https://restcountries.com/v3.1/all", {
       mode: "cors",
@@ -15,30 +17,33 @@ async function showCountry() {
       name: country.name.common,
       flag: country.flags.png,
     }));
-    countryContainer.classList.add("none");
-    h3.style.display = "flex";
+    card.classList.remove("none");
+    generateMessage.style.display = "flex";
     card.innerHTML = "";
+    btn.removeEventListener("click", showCountry);
 
     setTimeout(() => {
-      countryContainer.classList.remove("none");
-      h3.style.display = "none";
+      card.classList.add("none");
+      generateMessage.style.display = "none";
       const randomIndex = Math.floor(Math.random() * countries.length);
       const randomCountry = countries[randomIndex];
-
-      const countryName = document.createElement("h1");
+      const countryName = document.createElement("h2");
+      countryName.classList.add("country-name");
       countryName.textContent = randomCountry.name;
 
       const countryFlag = document.createElement("img");
+      countryFlag.classList.add("country-flag");
       countryFlag.src = randomCountry.flag;
 
       card.appendChild(countryName);
       card.appendChild(countryFlag);
       countryContainer.appendChild(card);
+      btn.addEventListener("click", showCountry);
     }, 2000);
   } catch (err) {
     console.log("Error fetch:", err);
+    btn.addEventListener("click", showCountry);
   }
 }
-showCountry();
 
 btn.addEventListener("click", showCountry);
