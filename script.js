16606d7a2938cfbe17e9b9e8e3ca4797cad6de0a
Ozod <bukhorizodao@gmail.com>
2023-08-07 13:36:18 +0500

    created a card where countries are stored from API call

--- script.js
+++ script.js
@@ -1,40 +1,44 @@
-// const countryContainer = document.querySelector(".country-container");
-// const btn = document.querySelector(".btn");
-// const h3 = document.querySelector("h3");
+const countryContainer = document.querySelector(".country-container");
+const card = document.querySelector(".card");
+const btn = document.querySelector(".btn");
+const h3 = document.querySelector("h3");
 
-// async function showCountry() {
-//   try {
-//     const response = await fetch(" https://restcountries.com/v3.1/all", {
-//       mode: "cors",
-//     });
-//     const data = await response.json();
-//     console.log(data);
+async function showCountry() {
+  try {
+    const response = await fetch(" https://restcountries.com/v3.1/all", {
+      mode: "cors",
+    });
+    const data = await response.json();
+    console.log(data);
 
-//     const countries = data.map((country) => ({
-//       name: country.name.common,
-//       flag: country.flags.png,
-//     }));
-//     h3.style.display = "flex";
-//     countryContainer.innerHTML = "";
+    const countries = data.map((country) => ({
+      name: country.name.common,
+      flag: country.flags.png,
+    }));
+    countryContainer.classList.add("none");
+    h3.style.display = "flex";
+    card.innerHTML = "";
 
-//     setTimeout(() => {
-//       h3.style.display = "none";
-//       const randomIndex = Math.floor(Math.random() * countries.length);
-//       const randomCountry = countries[randomIndex];
+    setTimeout(() => {
+      countryContainer.classList.remove("none");
+      h3.style.display = "none";
+      const randomIndex = Math.floor(Math.random() * countries.length);
+      const randomCountry = countries[randomIndex];
 
-//       const countryName = document.createElement("h1");
-//       countryName.textContent = randomCountry.name;
+      const countryName = document.createElement("h1");
+      countryName.textContent = randomCountry.name;
 
-//       const countryFlag = document.createElement("img");
-//       countryFlag.src = randomCountry.flag;
+      const countryFlag = document.createElement("img");
+      countryFlag.src = randomCountry.flag;
 
-//       countryContainer.appendChild(countryName);
-//       countryContainer.appendChild(countryFlag);
-//     }, 2000);
-//   } catch (err) {
-//     console.log("Error fetch:", err);
-//   }
-// }
-// showCountry();
+      card.appendChild(countryName);
+      card.appendChild(countryFlag);
+      countryContainer.appendChild(card);
+    }, 2000);
+  } catch (err) {
+    console.log("Error fetch:", err);
+  }
+}
+showCountry();
 
-// btn.addEventListener("click", showCountry);
+btn.addEventListener("click", showCountry);
