fa69ac05740c43d5e893371ee6ea0fc7217a2094
Ozod <bukhorizodao@gmail.com>
2023-08-07 12:25:33 +0500

    getting country names with their flags

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
+const countryContainer = document.querySelector('.country-container');
+const btn = document.querySelector('.btn');
+
 async function showCountry() {
+    try {
     const response = await fetch(" https://restcountries.com/v3.1/all", { mode: "cors"});
     const data = await response.json();
     console.log(data);
+
+    const countries = data.map(country =>  ({
+        name: country.name.common,
+        flag: country.flags.png,
+    }));
+
+    const randomIndex = Math.floor(Math.random() * countries.length);
+    const randomCountry = countries[randomIndex];
+
+    const countryName = document.createElement("h1")
+    countryName.textContent = randomCountry.name;
+
+    const countryFlag = document.createElement("img");
+    countryFlag.src = randomCountry.flag;
+
+    countryContainer.innerHTML = "";
+
+    countryContainer.appendChild(countryName);
+    countryContainer.appendChild(countryFlag);
+    } catch (err) {
+        console.log("Error fetch:", err);
+
+    }
 }
 showCountry();
+
+btn.addEventListener("click", showCountry);
